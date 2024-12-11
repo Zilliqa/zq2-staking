9adf8782326a5a7894b9522c12f571b059fd58f0
Łukasz Kosiak <kosiak.lukasz@gmail.com>
2024-12-11 17:45:58 +0100

    report

--- src/misc/stakingAbis.ts
+++ src/misc/stakingAbis.ts
@@ -28,6 +28,9 @@ export const depositAbi = [
 ]
 
 export const delegatorAbi = [
+  /**
+   * from Delegation.sol
+   */
   {
     "inputs": [],
     "name": "stake",
@@ -50,38 +53,36 @@ export const delegatorAbi = [
   },
   {
     "inputs": [],
+    "name": "claim",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    // proposal to add this so app can inform user about the pending claims
+    "inputs": [],
+    "name": "getPendingClaims",
+    "outputs": [
+      {
+        "internalType": "uint256[][]",
+        "name": "",
+        "type": "uint256[][]"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  }
+  /**
+   * From ILiquidDelegation.sol
+   */
+  {
+    "inputs": [],
     "name": "getLST",
     "outputs": [
       {
         "internalType": "address",
         "name": "",
         "type": "address"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "MIN_DELEGATION",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getCommissionNumerator",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
       }
     ],
     "stateMutability": "view",
@@ -100,13 +101,59 @@ export const delegatorAbi = [
     "stateMutability": "view",
     "type": "function"
   },
+  /**
+   * From BaseDelegation.sol
+   */
   {
+    // proposal: add to the Delegation.sol interface: getMinDelegation() returns(uint256)
+    "inputs": [],
+    "name": "MIN_DELEGATION",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    // proposal: add to the Delegation.sol interface: getCommission() returns(uint256, uint256) // (numerator, denumenator)
+    "inputs": [],
+    "name": "getCommissionNumerator",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    // proposal: add to the Delegation.sol interface: getStake() returns(uint256)
     "inputs": [],
     "name": "getStake",
     "outputs": [
       {
         "internalType": "uint256",
         "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    // proposal: add to the Delegation.sol interface: getClaimable() returns(uint256)
+    "inputs": [],
+    "name": "getClaimable",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "total",
         "type": "uint256"
       }
     ],
