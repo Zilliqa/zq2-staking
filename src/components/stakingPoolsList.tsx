3e113abd1bfd44cc838e85f0abb543efc2675927
GeorginaShall <georgina.shall@creoshift.com>
2025-02-11 16:12:08 +0200

    APT-1695-APT-1697

--- src/components/stakingPoolsList.tsx
+++ src/components/stakingPoolsList.tsx
@@ -113,8 +113,8 @@ const StakingPoolsList: React.FC = () => {
         </div>
 
         <div
-          className="grid grid-cols-1 gap-2.5 lg:gap-4 overflow-y-auto max-h-[calc(90vh-25vh)]
-            scrollbar-thin scrollbar-thumb-gray1 scrollbar-track-gray1 hover:scrollbar-thumb-gray1 pb-20 pr-2 lg:pr-4"
+          className="grid grid-cols-1 gap-2.5 lg:gap-4 overflow-y-auto max-h-[calc(90vh-38vh)] lg:max-h-[calc(90vh-25vh)]
+            scrollbar-thin scrollbar-thumb-gray1 scrollbar-track-gray1 hover:scrollbar-thumb-gray1 pb-4 lg:pb-20 pr-2 lg:pr-4"
         >
           {sortedLiquidStakingPoolsData.map(({ stakingPool, userData }) => (
             <StakingPoolCard
