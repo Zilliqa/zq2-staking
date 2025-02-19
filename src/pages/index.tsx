9ea883a0003010c0a891224886042a4389d4718a
Lukasz Kosiak <kosiak.lukasz@gmail.com>
2025-02-19 15:16:46 +0100

    zil giveaway added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ import ArrowBackAqua from "../assets/svgs/arrow-back-aqua.svg"
 import Logo from "../assets/svgs/logo.svg"
 import Star from "../assets/svgs/star.svg"
 import CustomWalletConnect from "@/components/customWalletConnect"
+import ZilGiveaway from "@/components/zilGiveaway"
 
 const HomePage = () => {
   const [isVisible, setIsVisible] = useState(false)
@@ -302,7 +303,12 @@ const HomePage = () => {
               />
             </div>
 
-            <div className="flex items-center space-x-4">
+            <div className="flex gap-2.5">
+              {
+                isWalletConnected && (
+                  <ZilGiveaway />
+                )
+              }
               <CustomWalletConnect notConnectedClassName="btn-primary-gradient-aqua sm:px-10 w-full sm:max-w-fit">
                 Connect wallet
               </CustomWalletConnect>
