4adc9dd52e37bc6269302fa790cc861eb07b4eb9
GeorginaShall <123496642+GeorginaShall@users.noreply.github.com>
2025-02-20 14:21:36 +0200

    Apt 1752 cont : scrollbar of non liquid purple  (#79)
    * APT-1710 remove white from gradient on hover and click validator card
    
    * APT-1752 scrollbars
    
    * APT-1752 lint
    
    * APT-1752 available rew

--- src/components/FastFadeScroll.tsx
+++ src/components/FastFadeScroll.tsx
@@ -1,15 +1,18 @@
+import { StakingPoolType } from "@/misc/stakingPoolsConfig"
 import { useState, useEffect, ReactNode } from "react"
 
 interface FastFadeScrollProps {
   children: ReactNode
   fadeDuration?: number
   className?: string
+  isPoolLiquid?: StakingPoolType
 }
 
 const FastFadeScroll = ({
   children,
   fadeDuration = 1000,
   className = "",
+  isPoolLiquid,
 }: FastFadeScrollProps) => {
   const [isScrolling, setIsScrolling] = useState(false)
   let scrollTimeout: any
@@ -27,10 +30,14 @@ const FastFadeScroll = ({
     return () => clearTimeout(scrollTimeout)
   }, [])
 
+  const isPoolLiquidBool = () => isPoolLiquid === StakingPoolType.LIQUID
+  console.log(isPoolLiquid)
+
   return (
     <div
       onScroll={handleScroll}
-      className={`${className} ${isScrolling ? "scrollbar-visible" : "scrollbar-hidden"} scrollbar-gradient`}
+      className={`${className} ${isScrolling ? "scrollbar-visible" : "scrollbar-hidden"}
+        ${isPoolLiquidBool() ? "scrollbar-aqua" : "scrollbar-purple"}`}
     >
       {children}
     </div>
--- src/components/stakingPoolsList.tsx
+++ src/components/stakingPoolsList.tsx
@@ -120,7 +120,10 @@ const StakingPoolsList: React.FC<StakingPoolsListProps> = ({
           />
         </div>
 
-        <FastFadeScroll className="flex-1 pb-8 lg:pb-4 mb-16 md:mb-0 overflow-y-scroll">
+        <FastFadeScroll
+          isPoolLiquid={stakingPoolForView?.stakingPool.definition.poolType}
+          className="flex-1 pb-8 lg:pb-4 mb-16 md:mb-0 overflow-y-scroll"
+        >
           <div className="grid grid-cols-1 gap-2.5 lg:gap-4 4k:gap-5">
             {sortedLiquidStakingPoolsData.map(({ stakingPool, userData }) => (
               <StakingPoolCard
