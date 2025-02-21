a305f2c5a58a305d6616e4ebeb90dd8d63e8c7e9
GeorginaShall <georgina.shall@creoshift.com>
2025-02-21 13:29:58 +0200

    APT-1811

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-S4YVQXKJY8" />
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S4YVQXKJY8');
+          `}
+        </Script>
+
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
