81a14e2b9f2479380fa3f5d1508fd2e641d703f0
GeorginaShall <georgina.shall@creoshift.com>
2025-03-06 16:58:33 +0200

    row to column btns

--- src/components/withdrawUnstakedZilPanel.tsx
+++ src/components/withdrawUnstakedZilPanel.tsx
@@ -99,7 +99,7 @@ const WithdrawZilPanel: React.FC<WithdrawZilPanelProps> = ({
             ) : (
               <div className="loading-blur"> 00000 zil</div>
             )}
-            <div className="max-lg:gap-2.5 max-lg:flex lg:w-1/3 lg:max-w-[250px] w-full">
+            <div className=" lg:w-1/3 max-w-[150px] sm:max-w-[250px] w-full">
               {getMinimalPoolStakingAmount(reward.address) >
               reward.zilRewardAmount ? (
                 <Tooltip
@@ -214,7 +214,7 @@ const WithdrawZilPanel: React.FC<WithdrawZilPanelProps> = ({
               ) : (
                 <div className="loading-blur">000.000 ZIL</div>
               )}
-              <div className="max-lg:gap-2.5 max-lg:flex lg:w-1/3 max-w-[250px] w-full">
+              <div className="max-lg:gap-2.5 max-lg:flex lg:w-1/3 max-w-[150px] sm:max-w-[250px] w-full">
                 <Button
                   className={` 
                     ${
