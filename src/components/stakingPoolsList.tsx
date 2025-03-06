2c9683007e05ed187c88dc57a400ed9bfc282a9e
GeorginaShall <georgina.shall@creoshift.com>
2025-03-06 13:53:39 +0200

    liquid non liquid hover and click

--- src/components/stakingPoolsList.tsx
+++ src/components/stakingPoolsList.tsx
@@ -85,10 +85,7 @@ const StakingPoolsList: React.FC<StakingPoolsListProps> = ({
 
   return (
     <>
-      <nav
-        aria-label="Tabs"
-        className="border-b-[0.5px] border-b-gray2 w-full flex "
-      >
+      <nav aria-label="Tabs" className="w-full flex ">
         {tabs.map((tab, index) => (
           <Tooltip
             placement="top"
@@ -99,9 +96,11 @@ const StakingPoolsList: React.FC<StakingPoolsListProps> = ({
             key={index}
           >
             <button
-              className={`w-1/2 whitespace-nowrap border-b-[0.5px] py-3 4k:py-4
-              ${selectedPoolType === tab.type ? `bold33 ${tab.type === StakingPoolType.LIQUID ? "border-aqua1" : "border-purple4"}` : "bold26 text-gray8 border-transparent"}
-            `}
+              className={`w-1/2 whitespace-nowrap py-3 4k:py-4 border-solid border-b transition-all duration-400 ease-in-out relative bold33 ${
+                selectedPoolType === tab.type
+                  ? ` text-white1 border-black2${tab.type === StakingPoolType.LIQUID ? " after:bg-aqua1" : " after:bg-purple4"} after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 after:animate-grow-width after:transition-all after:duration-300 before:bg-white before:absolute before:h-[1px] before:w-full before:bottom-0 before:left-0`
+                  : "text-gray1 border-black2 hover:text-white after:bg-white after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300"
+              } `}
               onClick={() => {
                 setSelectedPoolType(tab.type)
                 selectStakingPoolForView(null)
